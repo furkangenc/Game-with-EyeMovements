2912e7e1c199bb20840e65ca5352bd933870631c
Furkan Genc <frkngnc@gmail.com>
2015-03-31 12:15:00 +0300

    Cube controlled by Eye Movements

---
+++ Assets/SpinOnGaze.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpinOnGaze : MonoBehaviour 
+{
+	private GazeAwareComponent _gazeAware;
+	
+	void Start () 
+	{
+		_gazeAware = GetComponent<GazeAwareComponent>();
+	}
+	
+	void Update () 
+	{
+		if (_gazeAware.HasGaze) 
+		{
+			transform.Rotate(Vector3.forward);
+			transform.Rotate(Vector3.forward);
+		}
+	}
+}
